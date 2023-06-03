8f3b46ae3c49d4222fe359dc238d4327555354a6
Theo011 <teodragon97@hotmail.gr>
2023-06-03 20:58:29 +0300

    IPInfoProvider

---
+++ IP-Batch-API/IPInfoProvider/IPInfoProvider.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IPInfoProvider
+{
+    public interface IIPInfoProvider
+    {
+        Task<IPDetails> GetDetails(string ip);
+    }
+
+    public interface IPDetails
+    {
+        string City { get; set; }
+        string Country { get; set; }
+        string Continent { get; set; }
+        double Latitude { get; set; }
+        double Longitude { get; set; }
+    }
+
+    public class IPServiceNotAvailableException : ApplicationException
+    {
+        public IPServiceNotAvailableException(string message, Exception inner) : base(message, inner) { }
+    }
+
+    public class IPInfoProvider : IIPInfoProvider
+    {
+        private readonly HttpClient client;
+        private readonly string apiKey;
+
+        public IPInfoProvider(string apiKey)
+        {
+            client = new HttpClient
+            {
+                BaseAddress = new Uri("https://api.ipstack.com/")
+            };
+            this.apiKey = apiKey ?? throw new ArgumentNullException(nameof(apiKey));
+        }
+
+        public async Task<IPDetails> GetDetails(string ip)
+        {
+            try
+            {
+                string uri = $"https://api.ipstack.com/{ip}?access_key={apiKey}";
+
+                HttpResponseMessage response = await client.GetAsync(uri).ConfigureAwait(false);
+
+                string json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                IPDetails data = JsonConvert.DeserializeObject<IPDetails>(json)!;
+
+                return data;
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new IPServiceNotAvailableException("Service is not available.", ex);
+            }
+        }
+    }
+}
