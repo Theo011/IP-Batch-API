2c60550ee14cfe5569e7dda5d4d92c7d14ae834f
Theo011 <teodragon97@hotmail.gr>
2023-07-03 19:43:15 +0300

    batching (part 3) done
    The code as a whole (with some exceptions) needs error handling. The jobs dictionary never gets cleared which could be a problem if the system runs for too long. The update task starts instantly which could lead to problems.

--- IP-Batch-API/IP-Batch-API/Controllers/IPDetailsController.cs
+++ IP-Batch-API/IP-Batch-API/Controllers/IPDetailsController.cs
@@ -16,13 +16,16 @@ namespace IP_Batch_API.Controllers
         private readonly IMemoryCache _memoryCache;
         private readonly IMapper _mapper;
         private readonly IIPBatchRepository _iPBatchRepository;
+        private static Dictionary<Guid, Job> jobs = new();
+        private readonly IServiceScopeFactory _serviceScopeFactory;
 
-        public IPDetailsController(IIPInfoProvider iPInfoProvider, IMemoryCache memoryCache, IMapper mapper, IIPBatchRepository iPBatchRepository)
+        public IPDetailsController(IIPInfoProvider iPInfoProvider, IMemoryCache memoryCache, IMapper mapper, IIPBatchRepository iPBatchRepository, IServiceScopeFactory serviceScopeFactory)
         {
             _iPInfoProvider = iPInfoProvider ?? throw new ArgumentNullException(nameof(iPInfoProvider));
             _memoryCache = memoryCache ?? throw new ArgumentNullException(nameof(memoryCache));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _iPBatchRepository = iPBatchRepository ?? throw new ArgumentNullException(nameof(iPBatchRepository));
+            _serviceScopeFactory = serviceScopeFactory;
         }
 
         [HttpGet("{ip}")]
@@ -76,6 +79,108 @@ namespace IP_Batch_API.Controllers
             {
                 return StatusCode(503);
             }
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(List<IPDetailDto> ipDetailsDto)
+        {
+            var job = new Job
+            {
+                Id = Guid.NewGuid(),
+                Ips = ipDetailsDto.Select(x => x.Ip).ToList(),
+                TotalItems = ipDetailsDto.Count,
+                ProcessedItems = 0
+            };
+
+            // Console.WriteLine($"Created job with ID {job.Id}");
+
+            jobs.Add(job.Id, job);
+            Task.Run(() => ProcessJob(job));
+
+            return Ok(job.Id);
+        }
+
+        private async void ProcessJob(Job job)
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var iPBatchRepository = scope.ServiceProvider.GetRequiredService<IIPBatchRepository>();
+                var batchSize = 10;
+
+                while (job.Ips.Any())
+                {
+                    var batch = job.Ips.Take(batchSize).ToList();
+
+                    foreach (var ip in batch)
+                    {
+                        IPDetail ipDetails;
+
+                        if (_memoryCache.TryGetValue(ip, out IPDetail cachedIPDetails))
+                        {
+                            ipDetails = cachedIPDetails;
+
+                            var updatedDetails = await _iPInfoProvider.GetDetails(ip);
+
+                            if (updatedDetails is not null)
+                            {
+                                ipDetails = _mapper.Map(updatedDetails, ipDetails);
+                                iPBatchRepository.UpdateIPDetails(ipDetails);
+                                await iPBatchRepository.SaveChangesAsync();
+                            }
+                        }
+                        else if (await iPBatchRepository.GetIPDetailsAsync(ip) is IPDetail ipDetailsFromDb)
+                        {
+                            ipDetails = ipDetailsFromDb;
+
+                            var updatedDetails = await _iPInfoProvider.GetDetails(ip);
+
+                            if (updatedDetails is not null)
+                            {
+                                ipDetails = _mapper.Map(updatedDetails, ipDetails);
+                                iPBatchRepository.UpdateIPDetails(ipDetails);
+                                await iPBatchRepository.SaveChangesAsync();
+                            }
+                        }
+                        else if (await _iPInfoProvider.GetDetails(ip) is IPDetails ipDetailsFromProvider)
+                        {
+                            ipDetails = _mapper.Map<IPDetail>(ipDetailsFromProvider);
+                            ipDetails.Ip = ip;
+                            await iPBatchRepository.AddIPDetails(ipDetails);
+                            await iPBatchRepository.SaveChangesAsync();
+                        }
+                        else
+                        {
+                            continue;
+                        }
+
+                        _memoryCache.Set(ip, ipDetails, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(1)));
+                        job.ProcessedItems++;
+                    }
+
+                    job.Ips = job.Ips.Skip(batchSize).ToList();
+                }
+            }
+        }
+
+        [HttpGet("getProgress/{id}")]
+        public IActionResult GetProgress(Guid id)
+        {
+            // Console.WriteLine($"Getting progress for job with ID {id}");
+
+            if (jobs.ContainsKey(id))
+            {
+                var job = jobs[id];
+
+                // Console.WriteLine($"Job with ID {id} has {job.ProcessedItems}/{job.TotalItems} processed items");
+
+                return Ok($"{job.ProcessedItems}/{job.TotalItems}");
+            }
+            /*else
+            {
+                Console.WriteLine($"Job with ID {id} not found");
+            }*/
+
+            return NotFound();
         }
     }
 }
---
+++ IP-Batch-API/IP-Batch-API/Models/Job.cs
@@ -0,0 +1,10 @@
+﻿namespace IP_Batch_API.Models
+{
+    public class Job
+    {
+        public Guid Id { get; set; }
+        public List<string> Ips { get; set; } = null!;
+        public int TotalItems { get; set; }
+        public int ProcessedItems { get; set; }
+    }
+}
